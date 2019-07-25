c5d72cfeceea6b0459ea288b8245d04487b25fb8
Nerldy_ <arnodhis@gmail.com>
2019-07-25 10:40:02 +0300

    finished ex2

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
-	x := 42
-	y := "James Bond"
-	z := true
-	fmt.Println(x, y, z)
+var x int
+var y string
+var z bool
 
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
 }
