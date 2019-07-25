16ef85ac5e0f8b8ded6e7f699fc35c0c5df3c343
Nerldy_ <arnodhis@gmail.com>
2019-07-25 15:12:23 +0300

    finished ex5

--- hello.go
+++ hello.go
@@ -6,9 +6,14 @@ type NUMBER int
 
 var x NUMBER
 
+var y int
+
 func main() {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
 	fmt.Println(x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 }
